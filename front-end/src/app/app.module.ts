8ccbfe30015c87960c8320dc8c777e3cb14cbafe
siravor <luis.sr@aluno.ifsc.edu.br>
2020-04-20 12:22:13 -0300

    commit na master foi mal

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './main/pages/home-page/home-page.component';
+import { LoginPageComponent } from './main/pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,

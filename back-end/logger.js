f201b869e2b91127a6db03e656f52ad8f7de3360
ciochetta <luisfelipeciochetta@gmail.com>
2020-04-22 21:24:56 -0300

    fix seguradoras

--- back-end/logger.js
+++ back-end/logger.js
@@ -1,3 +1,4 @@
 module.exports = {
-  log: function (to_log) { if(global.env === 'dev') console.log(to_log); }
-}  
+  log: function (to_log) { if(global.env === 'dev') console.log(to_log); },
+  error: function (to_error) { if(global.env === 'dev') console.error(to_error); }
+}

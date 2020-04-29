53929a8d5eaa71560f38e2329433edcea241da47
siravor <luis.sr@aluno.ifsc.edu.br>
2020-04-29 15:03:33 -0300

    update pdf

--- front-end/src/app/main/pages/seguradora-page/partials/seguradora-view/seguradora-view.component.ts
+++ front-end/src/app/main/pages/seguradora-page/partials/seguradora-view/seguradora-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import domtoimage from 'dom-to-image';
+import * as jsPDF from 'jspdf';
 import {SeguradoraService} from "../../../../services/seguradora.service";
 import {Router} from "@angular/router";
 
@@ -8,6 +10,10 @@ import {Router} from "@angular/router";
   styleUrls: ['./seguradora-view.component.scss']
 })
 export class SeguradoraViewComponent implements OnInit {
+
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
+
+
 
   seguradora: any;
 
@@ -19,6 +25,40 @@ export class SeguradoraViewComponent implements OnInit {
 
   ngOnInit() {
     this.seguradora = this.seguradoraService.getseguradoraInfoWithOutFormGroup();
+  }
+
+  downloadPDF()
+  {
+    var node = document.getElementById('parentdiv');
+    var img;
+    var filename;
+    var newImage;
+    domtoimage.toPng(node, { bgcolor: '#fff' })
+      .then(function(dataUrl) {
+        img = new Image();
+        img.src = dataUrl;
+        newImage = img.src;
+        img.onload = function(){
+          var pdfWidth = img.width;
+          var pdfHeight = img.height;
+          // FileSaver.saveAs(dataUrl, 'my-pdfimage.png'); // Save as Image
+          var doc;
+          if (pdfWidth > pdfHeight)
+          {
+            doc = new jsPDF('l', 'px', [pdfWidth , pdfHeight]);
+          } else {
+            doc = new jsPDF('p', 'px', [pdfWidth , pdfHeight]);
+          }
+          var width = doc.internal.pageSize.getWidth();
+          var height = doc.internal.pageSize.getHeight();
+          doc.addImage(newImage, 'PNG',  10, 10, width, height);
+          filename = 'mypdf_' + '.pdf';
+          doc.save(filename);
+        };
+      })
+      .catch(function(error) {
+        // Error Handling
+      });
   }
 
 }

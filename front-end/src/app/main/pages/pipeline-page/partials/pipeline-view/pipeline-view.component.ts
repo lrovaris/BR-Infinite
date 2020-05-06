5e749a9d01c8bef9537242db53d7163f8834b60e
siravor <luis.sr@aluno.ifsc.edu.br>
2020-05-06 09:48:38 -0300

    update - pipeline

--- front-end/src/app/main/pages/pipeline-page/partials/pipeline-view/pipeline-view.component.ts
+++ front-end/src/app/main/pages/pipeline-page/partials/pipeline-view/pipeline-view.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { CorretoraService} from "../../../../services/corretora.service";
+import { Router} from "@angular/router";
+import { ColaboradorService} from "../../../../services/colaborador.service";
+import domtoimage from 'dom-to-image';
+import * as jsPDF from 'jspdf';
+import { PipelineService} from "../../../../services/pipeline.service";
 
 @Component({
   selector: 'app-pipeline-view',
@@ -7,9 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipelineViewComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
+
+  oportunidade: any;
+
+  navigatePipeline() {
+    this.router.navigate(['pipeline'])
+  }
+
+  constructor(private oportunidadeService: PipelineService, private router: Router) { }
+
+  navigateEdit() {
+    this.oportunidadeService.setIsEditTrue();
+    this.router.navigate(['pipeline/cadastro'])
+  }
+
 
   ngOnInit() {
+    this.oportunidade = this.oportunidadeService.getOportunidadeWIthOutForm();
+    console.log(this.oportunidade);
+  }
+
+
+  downloadPDF() {
+    var node = document.getElementById('parentdiv');
+    var img;
+    var filename;
+    var newImage;
+    domtoimage.toPng(node, { bgcolor: '#fff' })
+      .then(function(dataUrl) {
+        img = new Image();
+        img.src = dataUrl;
+        newImage = img.src;
+        img.onload = function(){
+          var pdfWidth = img.width;
+          var pdfHeight = img.height;
+          // FileSaver.saveAs(dataUrl, 'my-pdfimage.png'); // Save as Image
+          var doc;
+          if (pdfWidth > pdfHeight)
+          {
+            doc = new jsPDF('l', 'px', [pdfWidth , pdfHeight]);
+          } else {
+            doc = new jsPDF('p', 'px', [pdfWidth , pdfHeight]);
+          }
+          var width = doc.internal.pageSize.getWidth();
+          var height = doc.internal.pageSize.getHeight();
+          doc.addImage(newImage, 'PNG',  10, 10, width, height);
+          filename = 'mypdf_' + '.pdf';
+          doc.save(filename);
+        };
+      })
+      .catch(function(error) {
+        // Error Handling
+      });
   }
 
 }

b0961ec83874127d075127fbab0af3393b497903
siravor <luis.sr@aluno.ifsc.edu.br>
2020-05-25 17:27:33 -0300

    update

--- front-end/src/app/main/pages/producao-page/producao-page.component.ts
+++ front-end/src/app/main/pages/producao-page/producao-page.component.ts
@@ -55,7 +55,6 @@ export class ProducaoPageComponent implements OnInit {
           prod.seguradora = seguradora;
           return prod;
         });
-        console.log(this.allProducoes);
       });
       this.corretoraService.getAllCorretoras().subscribe((cor_data:any) => {
         this.corretora = cor_data;
@@ -70,8 +69,7 @@ export class ProducaoPageComponent implements OnInit {
           prod.corretora = corretora;
           return prod;
         });
-        console.log(this.allProducoes);
-        this.calculateDatesCorretora(this.allProducoes);
+        // this.calculateDatesCorretora(this.allProducoes);
       });
 
     })
@@ -79,7 +77,79 @@ export class ProducaoPageComponent implements OnInit {
   } // FIM DO NG ON INIT (bem grandinho ne rs :3)
 
 
+  getCorretoraLineInfo(id, seguradoraProductionArray) {
+   let corretoraProductionArray = [];
+    for (let i = 0; i < seguradoraProductionArray.length; i++) {
+      if (seguradoraProductionArray[i].corretora._id === id) {
+        corretoraProductionArray.push(seguradoraProductionArray[i]);
+      }
+    }
 
+    let dateToReturn = corretoraProductionArray[0].date;
+    let nameToReturn = corretoraProductionArray[0].corretora.name;
+    let totalToReturn = corretoraProductionArray[0].total;
+    let projectionToReturn = 0;
+
+   let corretoraDates = this.calculateDatesCorretora(corretoraProductionArray);
+
+   console.log(corretoraDates);
+
+   let montanteAdicional = 0;
+
+   let timesCounted = 0;
+
+   let media: number;
+
+   let minorDate = corretoraProductionArray[0].date;
+
+    for (let i = 0; i < corretoraProductionArray.length; i++) {
+
+      if (this.isTheNewDateBigger(dateToReturn, corretoraProductionArray[i].date)) {
+        dateToReturn = corretoraProductionArray[i].date;
+
+        montanteAdicional = montanteAdicional + (corretoraProductionArray[i].total - totalToReturn);
+
+        totalToReturn = corretoraProductionArray[i].total;
+
+        console.log(montanteAdicional);
+        timesCounted++
+
+      }
+
+      /*if (!this.isTheNewDateBigger(minorDate, corretoraProductionArray[i].date)) {
+        minorDate = corretoraProductionArray[i].date
+      }*/
+
+    }
+
+    // numeros dia dias uteis no mes é 20
+    // numero total de dias aproximado é 30
+    // portanto o numero de dias inuteis deve ser 10
+    // o numero de dias uteis faltando deve ser 30 - dataAtual - valor de dias inuteis restantes
+    // e o valor de dias inuteis restantes é igual a (30 - dataAtual) / 3
+
+
+
+    console.log('-------');
+    media = montanteAdicional / timesCounted;
+    console.log(media);
+    console.log('-------');
+
+
+
+    let diasFaltando = ((30 - this.getDateInfo(dateToReturn).day) * 2) / 3;
+    console.log(diasFaltando.toFixed());
+
+    projectionToReturn = (diasFaltando * media) + totalToReturn;
+
+    return {
+      media: media.toFixed(),
+      name: nameToReturn,
+      date: dateToReturn,
+      total: totalToReturn,
+      projection: projectionToReturn.toFixed()
+    }
+  }
 
   setActiveSeguradora(id) {
 
@@ -89,44 +159,34 @@ export class ProducaoPageComponent implements OnInit {
 
     this.activeSeguradora = id;
 
+    let activeSeguradoraProductionsArray = [];
+
     for (let i = 0; i < this.allProducoes.length; i++) {
-
       if (this.allProducoes[i].seguradora._id === this.activeSeguradora) {
-
-        this.seguradoraName = this.allProducoes[i].seguradora.name;
-
-        if (this.corretorasOfActiveSeguradora.length === 0) {
-
-          this.corretorasOfActiveSeguradora.push(this.allProducoes[i]);
-
-        } else {
-
-          let corretora = this.corretorasOfActiveSeguradora.find(cor => this.allProducoes[i].corretora._id === cor.corretora._id);
-
-          if (corretora === undefined) {
-
-            this.corretorasOfActiveSeguradora.push(this.allProducoes[i]);
-
-          } else {
-
-            if (this.isTheNewDateBigger(corretora.date, this.allProducoes[i].date)) {
-
-              let index = this.corretorasOfActiveSeguradora.indexOf(corretora);
-              if (index !== -1) this.corretorasOfActiveSeguradora.splice(index, 1);
-              this.corretorasOfActiveSeguradora.push(this.allProducoes[i]);
-
-            }
-
-          }
-
-        }
-
+        activeSeguradoraProductionsArray.push(this.allProducoes[i]);
       }
-
     }
 
-    console.log(this.corretorasOfActiveSeguradora);
-    console.log(this.arrayWithOldDatesProduction);
+    this.seguradoraName = activeSeguradoraProductionsArray[0].seguradora.name;
+
+    let allCorretoras = [];
+
+    for (let i = 0; i < activeSeguradoraProductionsArray.length; i++) {
+      let corretora = allCorretoras.find(cor => activeSeguradoraProductionsArray[i].corretora._id === cor);
+
+      if (corretora === undefined) {
+
+        allCorretoras.push(activeSeguradoraProductionsArray[i].corretora._id);
+
+      }
+    }
+
+    this.corretorasOfActiveSeguradora = [];
+
+
+    for (let i = 0; i < allCorretoras.length; i++) {
+      this.corretorasOfActiveSeguradora.push(this.getCorretoraLineInfo(allCorretoras[i], activeSeguradoraProductionsArray))
+    }
 
     for (let i = 0; i < this.corretorasOfActiveSeguradora.length; i++) {
 
@@ -145,6 +205,10 @@ export class ProducaoPageComponent implements OnInit {
   TODO _id: "5ec820db1b5b4a1ff5feb999"
   */
 
+
+
+
+
   calculateDatesCorretora(corretoraProduction) {
 
     let yearsObject: Object = {};
@@ -153,25 +217,17 @@ export class ProducaoPageComponent implements OnInit {
 
       let currentDate = this.getDateInfo(corretoraProduction[i].date);
 
-      if (Object.entries(yearsObject).length === 0) {
-
-        let year = currentDate.anoInfo;
-
-        yearsObject[year] = {};
-
-      } else {
-        let allYears = Object.entries(yearsObject)
-      }
+      yearsObject = this.makedataobj(currentDate, yearsObject);
 
     }
-    console.log(yearsObject)
+   return yearsObject;
   }
 
   getDateInfo(date) {
     return {
-      diaInfo: date.split("/")[0],
-      mesInfo: date.split("/")[1],
-      anoInfo: date.split("/")[2]
+      day: date.split("/")[0],
+      month: date.split("/")[1],
+      year: date.split("/")[2]
     }
   }
 
@@ -187,41 +243,91 @@ export class ProducaoPageComponent implements OnInit {
 
     // SEPARACAO DAS DATAS EM DIA MES E ANO PARA AS COMPARACOES
 
-    if (dataNew.anoInfo < dataOld.anoInfo) {
+    if (dataNew.year < dataOld.year) {
 
       return false // ANO DO NOVO DOCUMENTO É MENOR QUE O DOCUMENTO NO ARRAY LOGO NAO É A ULTIMA DATA
 
-    } else if ( (dataNew.anoInfo >= dataOld.anoInfo) ) {
+    } else if ( (dataNew.year >= dataOld.year) ) {
 
-      if (dataNew.mesInfo < dataOld.mesInfo) {
+      if (dataNew.month < dataOld.month) {
 
         return false  // MES DO NOVO DOCUMENTO É MENOR QUE O DOCUMENTO NO ARRAY LOGO NAO É A ULTIMA DATA
 
-      } else if ( dataNew.mesInfo >= dataOld.mesInfo) {
+      } else if ( dataNew.month >= dataOld.month) {
 
-        if (dataNew.diaInfo < dataOld.diaInfo) {
+        if (dataNew.day < dataOld.day) {
 
           return false   // DIA DO NOVO DOCUMENTO É MENOR QUE O DOCUMENTO NO ARRAY LOGO NAO É A ULTIMA DATA
 
-        } else if ( dataNew.diaInfo > dataOld.diaInfo ) {
+        } else if ( dataNew.day > dataOld.day ) {
 
           return true
 
         }
 
-        if (dataNew.mesInfo > dataOld.mesInfo) {
+        if (dataNew.month > dataOld.month) {
           return true
         }
 
       }
 
-      return dataNew.anoInfo > dataOld.anoInfo;
+      return dataNew.year > dataOld.year;
 
     }
 
 
   }
 
+
+
+  makedataobj(new_data, yearsObj) {
+
+    let yearsArray = Object.entries(yearsObj);
+
+    if(yearsArray.length === 0){
+
+      yearsObj[new_data.year] = {}
+
+    }else {
+
+      let currentYearObj = yearsArray.find(year_obj => year_obj[0] === new_data.year)
+
+      if(currentYearObj !== undefined){
+        let monthsArray = Object.entries(currentYearObj[1]);
+
+        if(monthsArray.length === 0){
+
+          yearsObj[new_data.year][new_data.month] = [new_data.day]
+
+        }else {
+
+          let currentMonthObj = monthsArray.find(month_obj => month_obj[0] ===new_data.month)
+
+          if(currentMonthObj !== undefined){
+
+            let daysArray = currentMonthObj[1];
+
+            if (daysArray.length === 0) {
+              yearsObj[new_data.year][new_data.month].push(new_data.day);
+            }
+
+            else if(!daysArray.includes(new_data.day)){
+
+              yearsObj[new_data.year][new_data.month].push(new_data.day);
+
+            }
+          }else {
+            yearsObj[new_data.year][new_data.month] = [new_data.day]
+          }
+        }
+      }
+      else{
+        yearsObj[new_data.year] = {}
+        yearsObj[new_data.year][new_data.month] = [new_data.day]
+      }
+    }
+    return yearsObj;
+  }
 
 
 }
